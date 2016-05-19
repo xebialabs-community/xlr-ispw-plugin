f2041eb85ce719b3a6f22f40f2b5c4dc261cf75c
jdewinne <joris.dewinne@gmail.com>
2016-05-19 11:08:49 -0700

    Adding additional log output

--- src/main/resources/xlrelease/phpRequest.py
+++ src/main/resources/xlrelease/phpRequest.py
@@ -1,6 +1,7 @@
 
 from java.io import File
 from net.collegeman.phpinjava import PHP
+from org.apache.commons.io import FileUtils
 
 
 arguments = []
@@ -9,8 +10,12 @@ props = getCurrentTask().getPythonScript().getInputProperties()
 for k in props:
     arguments.append("%s=%s" % (k.name,getCurrentTask().getPythonScript().getProperty(k.name)))
 
-php = PHP(File("%s/%s" % (phpHomeFolder,phpScript)), logFileName, arguments)
-
-output = php.toString()
-
-print "We received the following [%s]" % output
+php = None
+try:
+    php = PHP(File("%s/%s" % (phpHomeFolder,phpScript)), logFileName, arguments)
+except:
+    log_output = FileUtils.readFileToString(File("%s/%s.log" % (phpHomeFolder,logFileName)))
+    print "The log file contains: %s" % log_output
+finally:
+    output = php.toString()
+    print "We received the following through php [%s]" % output
