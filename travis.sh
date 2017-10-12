65c60e758bc4cc76c56fecd1e2887e88659fec14
Joris De Winne <joris.dewinne@gmail.com>
2017-10-12 02:12:10 -0700

    Using releases docker container

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-docker run -d -p 28080:8080 xebialabs/xl-docker-demo-ispw
+docker run -d -p 28080:8080 xebialabs/xl-docker-demo-ispw:releases
 ./gradlew compileDocker
 
