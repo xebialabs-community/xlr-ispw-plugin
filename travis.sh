f6c9ca16cf5216bed7891585d27496f40bce4fa3
Vincent Lussenburg <vlussenburg@users.noreply.github.com>
2019-08-08 15:06:49 -0600

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-docker run --rm -d -p 28080:8080 --name ispw xebialabs/xl-docker-demo-ispw:latest
+docker run --rm -d -p 28080:8080 --name ispw xebialabsunsupported/xl-docker-demo-ispw:latest
 ./gradlew compileDocker
 docker stop ispw
 
